f83de4a3a7f48003f554cd16a0131b03836923ac
Arpan Tripathi <tripathiarpan20@gmail.com>
2025-02-18 16:10:59 +0000

    weight setting regardless

--- auditing/audit.py
+++ auditing/audit.py
@@ -182,19 +182,20 @@ async def audit_weights(config: AuditConfig) -> bool:
 
     similarity_between_scores = _normalised_vector_dot_product(rayon_weights, weights_values)
 
+    my_vali_uid = None
+    if config.keypair:
+        logger.info(f"Hotkey and coldkey pair inputted, attempting to find a vali uid on netuid {PROD_NETUID}...")
+        _, my_vali_uid = query_substrate(
+            substrate, "SubtensorModule", "Uids", [config.netuid, config.keypair.ss58_address], return_value=True
+        )
+    
     if similarity_between_scores > 0.98:
         logger.info(f"✅ Yay! The scores are similar to the weights set on chain!! Similarity: {similarity_between_scores}")
 
-        if config.keypair:
-            logger.info(f"Hotkey and coldkey pair inputted, attempting to find a vali uid on netuid {PROD_NETUID}...")
-            _, my_vali_uid = query_substrate(
-                substrate, "SubtensorModule", "Uids", [config.netuid, config.keypair.ss58_address], return_value=True
-            )
-
-            if my_vali_uid is not None:
-                logger.info(f"Found my vali uid on netuid {PROD_NETUID}, setting weights!")
-                success = await set_weights(config, node_ids_formatted, node_weights_formatted, my_vali_uid)
-                return success
+        if config.keypair and my_vali_uid is not None:
+            logger.info(f"Found my vali uid on netuid {PROD_NETUID}, setting weights!")
+            success = await set_weights(config, node_ids_formatted, node_weights_formatted, my_vali_uid)
+            return success
         return True
 
     else:
@@ -202,6 +203,9 @@ async def audit_weights(config: AuditConfig) -> bool:
             f"Dear Auditor, the similarity between the scores and the weights set on chain is {similarity_between_scores}."
             "This is quite low, and you might want to look into this!"
         )
+        if config.keypair and my_vali_uid is not None:
+            logger.info(f"Found my vali uid on netuid {PROD_NETUID}, setting weights!")
+            success = await set_weights(config, node_ids_formatted, node_weights_formatted, my_vali_uid)
         return False
 
 
